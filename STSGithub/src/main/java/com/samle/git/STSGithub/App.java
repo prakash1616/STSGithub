b8e43352713b8d0b9aa058e16e8eb30ea47e8365
ptripathi26 <ptripathi26@INPF0YTP9T.CSCMWS.CSCMWS.COM>
2019-06-15 13:15:58 +0530

    first commit

---
+++ STSGithub/src/main/java/com/samle/git/STSGithub/App.java
@@ -0,0 +1,13 @@
+package com.samle.git.STSGithub;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
